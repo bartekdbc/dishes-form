ed4e32f01e1c74a7eb7771c2a89d6f3c1077897d
Bartlomiej Debiec <116588815+bartekdbc@users.noreply.github.com>
2023-03-19 23:16:13 +0700

    Container component implemented

---
+++ src/components/Container/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 650px;
+  flex-basis: 650px;
+  margin: 20px auto 0px;
+  padding: 30px;
+  border: 1px solid white;
+  border-radius: 10px;
+  box-shadow: black 0px 0px 20px;
+`;
