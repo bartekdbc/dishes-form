f017ad452d7c7c6df43d3fa0a57b3f4c6b7a9bc4
Bartlomiej Debiec <116588815+bartekdbc@users.noreply.github.com>
2023-03-20 18:13:11 +0700

    media queries and css styles added

--- src/components/Container/styled.js
+++ src/components/Container/styled.js
@@ -1,15 +1,13 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  /* max-width: 650px;
-  flex-basis: 650px;
-  margin: 20px auto 0px;
-  padding: 30px;
-  border: 1px solid white;
-  border-radius: 10px;
-  box-shadow: black 0px 0px 20px; */
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
+  color: ${({ theme }) => theme.color.black};
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.small}px) {
+    display: grid;
+  }
 `;
--- src/components/Form/styled.js
+++ src/components/Form/styled.js
@@ -1,28 +1,47 @@
 import styled from "styled-components";
 
-export const StyledForm = styled.div`
-  background-color: #ffffff;
-  border: 1px solid white;
+export const Wrapper = styled.div`
+  background-color: ${({ theme }) => theme.color.white};
+  border: 1px solid ${({ theme }) => theme.color.white};
   border-radius: 10px;
-  /* box-shadow: black 0px 0px 20px; */
-  padding: 30px;
+  display: flex;
+  align-items: center;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.medium}px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.small}px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
+
+export const StyledForm = styled.div`
+  padding: 30px 40px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.medium}px) {
+    padding: 20px 30px;
+  }
 `;
 
 export const Title = styled.h1`
   font-weight: 700px;
-  font-size: 22px;
+  font-size: 26px;
   padding-bottom: 2px;
+  margin-top: 0;
 `;
 
 export const ExtraTitle = styled.p`
   font-weight: 500;
   font-size: 16px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.medium}px) {
+    font-size: 14px;
+  }
 `;
 
 export const FieldWrapper = styled.div`
-  /* display: flex;
-  justify-content: space-between;
-  align-items: center; */
   margin-bottom: 16px;
 `;
 
@@ -34,41 +53,75 @@ export const StyledLabel = styled.label`
 `;
 
 export const StyledInput = styled.input`
-  width: 80%;
+  width: 100%;
   padding: 10px;
-  border: 2px solid #ddddee;
+  border: 2px solid ${({ theme }) => theme.color.snuff};
   border-radius: 5px;
+  transition: border 0.5s;
 
   &:focus {
     outline: none;
-    border: 2px solid #14b8a6;
+    border: 2px solid ${({ theme }) => theme.color.teal};
   }
 `;
 
 export const ErrorMessage = styled.span`
+  display: block;
+  font-size: 14px;
   font-weight: bold;
-  color: #800;
+  color: ${({ theme }) => theme.color.red};
+  margin-top: 5px;
+  transition: 0.3s;
+`;
+
+export const ButtonsWrapper = styled.div`
+  display: flex;
+  gap: 20px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.medium}px) {
+    flex-direction: column;
+    gap: 5px;
+  }
 `;
 
 export const Button = styled.button`
   border-radius: 5px;
   width: 100%;
   padding: 8px 15px;
-  /* width: 100%; */
-  border: 1px solid #14b8a6;
+  border: 1px solid ${({ theme }) => theme.color.teal};
   cursor: pointer;
-  background-color: #14b8a6;
-  color: white;
+  background-color: ${({ theme }) => theme.color.teal};
+  color: ${({ theme }) => theme.color.white};
   font-size: 16px;
   font-weight: bold;
   margin: 10px 0px 0px;
+  transition: filter 0.3s;
 
   &:hover {
-    background-color: blue;
+    filter: brightness(110%);
+  }
+
+  &:active {
+    filter: brightness(120%);
+  }
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.color.gray};
+    border: 1px solid ${({ theme }) => theme.color.gray};
+    cursor: not-allowed;
   }
 `;
 
-export const Image = styled.div`
-  background-image: url(${dishImage});
-  background-size: cover;
+export const StyledImage = styled.img`
+  max-width: 400px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.medium}px) {
+    max-width: 350px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.small}px) {
+    max-width: 300px;
+    display: block;
+    margin: 0 auto;
+  }
 `;
