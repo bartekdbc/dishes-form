78e8ea6cd295ad06b59c62e9ca7fcb26f25a8aba
Bartlomiej Debiec <116588815+bartekdbc@users.noreply.github.com>
2023-03-20 20:55:49 +0700

    url moved to separate const

--- src/components/Form/submitDish.js
+++ src/components/Form/submitDish.js
@@ -1,15 +1,14 @@
+const url = "https://umzzcc503l.execute-api.us-west-2.amazonaws.com/dishes/";
+
 const submitDish = async (values) => {
   try {
-    const response = await fetch(
-      "https://umzzcc503l.execute-api.us-west-2.amazonaws.com/dishes/",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(values),
-      }
-    );
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    });
 
     if (!response.ok) {
       throw new Error(response.statusText);
