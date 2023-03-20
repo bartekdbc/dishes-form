f2c0df54a84bfcc9c8a5258965dccd0ec767cd5d
Bartlomiej Debiec <116588815+bartekdbc@users.noreply.github.com>
2023-03-20 21:56:36 +0700

    GlobalStyle, theme and imgs moved to assets folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { GlobalStyle } from "./GlobalStyle";
+import { GlobalStyle } from "./assets/GlobalStyle";
 import { ThemeProvider } from "styled-components";
-import { theme } from "./theme";
+import { theme } from "./assets/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
