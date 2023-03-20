db90f469891a1a4649b141d8b2337f36da67fd75
Bartlomiej Debiec <116588815+bartekdbc@users.noreply.github.com>
2023-03-20 21:57:13 +0700

    submitDish updated

--- src/components/Form/submitDish.js
+++ src/components/Form/submitDish.js
@@ -1,24 +1,20 @@
 const url = "https://umzzcc503l.execute-api.us-west-2.amazonaws.com/dishes/";
 
-const submitDish = async (values) => {
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(values),
-    });
+const submitDish = (values) => {
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(values),
+  })
+    .then((response) => response.json())
+    .then((responseMessage) => serverResponse(responseMessage))
+    .catch((error) => console.error("Something bad happened!", error));
+};
 
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.error("Something bad happened!", error);
-  }
+const serverResponse = (response) => {
+  alert(`Server response: ${JSON.stringify(response)}`);
 };
 
 export default submitDish;
